5b55340d8a014c75ffbc63e8661ddacb49b3af0c
Jose Estrada <j.estrada49@icloud.com>
2023-10-28 21:56:06 +0000

    updating import order

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,8 +1,8 @@
 import type { MetaFunction } from "@remix-run/node";
 import { Link } from "@remix-run/react";
 
-import { useOptionalUser } from "~/utils";
 import { links } from "~/constants/links";
+import { useOptionalUser } from "~/utils";
 
 export const meta: MetaFunction = () => [{ title: "Remix Notes" }];
 
